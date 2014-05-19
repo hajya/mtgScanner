7f3fda760b6b007bf7f2fac1c2b3e266c3afbc5b
Ali Hajy <alihaji484@gmail.com>
2014-05-19 11:13:35 -0700

    working working working

--- imageScraper.py
+++ imageScraper.py
@@ -1,24 +1,66 @@
 #!/usr/bin/env python
 import json
-from os import path
-from pprint import pprint
+import os
 import urllib
+from PIL import ImageOps, Image
 
-bng = "BNG.json"
+from os import path
+
+
+
 allTheData = 'allMagicSets42014.json'
-mtgImageUrl = 'http://mtgimage.com/card/{0}.jpg'
+
+
 #extract imagenames
+def loadCardFromJson(jsonDB):
+	mtgImageUrl = 'http://mtgimage.com/card/{0}.jpg'
+	
+	df = open(path.join(path.dirname(__file__),'resources',bng))
+	data = json.load(df)
+	cardList=[]
+	for card in data['cards']:
+		#create proper URLs based on this pattern: http://mtgimage.com/card/Craw%20Wurm.jpg
+		cardList.append(mtgImageUrl.format(card['imageName']).replace(" ","_"))
+	return cardList
+	df.close()
+	
+def downloadAndGreyscaleCardImages(listOfCardNames,  cardImagePath):
+	if not path.exists(path.join(path.join(path.dirname(__file__),cardImagePath))):
+		os.makedirs(path.join(path.join(path.dirname(__file__),cardImagePath)))
+		
+	cardFileFullPathColor = '{0}\{1}'
+	
+	for card in listOfCardNames[1:5]:
+		print card
+		cardFileFullPathColor = cardFileFullPathColor.format(cardImagePath,card.split("/")[-1])
+		cardFileFullPathGrey =  cardFileFullPathColor.replace("jpg","png")
+		
+		print(cardFileFullPathColor)
+		print(cardFileFullPathGrey)
+		if not (path.isfile(cardFileFullPathGrey)):
+			urllib.urlretrieve(card, cardFileFullPathColor.format(cardImagePath,card.split("/")[-1]))
+			convertToGrey(cardFileFullPathColor, cardFileFullPathGrey)
+		os.remove(cardFileFullPathColor)
+		cardFileFullPathColor = '{0}\{1}'
+		
+			
+			
+def convertToGrey(colorCard, greyCard):
+	img = Image.open(colorCard)
+	img = ImageOps.grayscale(img)
+	img.save(greyCard)
+	return
+		
 
-df = open(path.join(path.dirname(__file__),'resources',bng))
-data = json.load(df)
-cardList=[]
-for card in data['cards']:
-    #create proper URLs based on this pattern: http://mtgimage.com/card/Craw%20Wurm.jpg
-    cardList.append(mtgImageUrl.format(card['imageName']))
+		
+		
+if __name__ == "__main__":
 
-df.close()
-
-urllib.urlretrieve(cardList[1],'image.jpg')
-
+	bng = "BNG.json"
+	cardImagePath = "cardImages"
+	cardList = loadCardFromJson(bng)
+	downloadAndGreyscaleCardImages(cardList, cardImagePath)
+	
+	
 
 
